c462e9f7a7d6d619455be5e4f92181a3af4d1032
Yathreb <yathrebamarneh93@gmail.com>
2020-08-19 16:49:53 +0300

    focusing feature

--- src/component/App.js
+++ src/component/App.js
@@ -4,6 +4,12 @@ import ButtonPanel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 import "./App.css";
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef()
+  }
+  
   state = {
     total: null,
     next: null,
@@ -23,16 +29,21 @@ export default class App extends React.Component {
     handleKey = event => {
       const keyPressedAllowed = ['0','1','2','3','4','5','6','7','8','9','c','%','/', 'x','-', '+', '=','.'];
       let KeyName = event.key;
+      console.log(event)
       if(keyPressedAllowed.includes(KeyName)) {
         this.setState(calculate(this.state, KeyName));
       }
      
     };
+
+  componentDidMount() {
+    this.inputRef.current.focus()
+  }
   
   render() {
     return (
-      <div className="component-app" tabIndex="0" onKeyUp={(e) => this.handleKey(e)}>
-        <Display value={this.state.next || this.state.total || "0"} />
+      <div className="component-app" tabIndex="0" onKeyUp={this.handleKey} ref= {this.inputRef}>
+        <Display value={this.state.next || this.state.total || "0"} autofocus />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
